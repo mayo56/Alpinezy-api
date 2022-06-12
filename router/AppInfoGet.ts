00bab4fb7aaf13b09661677c0ba8c5d15315c9ae
mayo56 <maxinou56.lr@gmail.com>
2022-06-12 13:16:37 +0200

    dsdq

--- router/AppInfoGet.ts
+++ router/AppInfoGet.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const AIG = express();
 
-
+import { AIGController } from "../controller/AIGController";
+AIG.get('/badge/:id',AIGController.getBadges);
 
 export default AIG;
